0fc7e928f6f17b34020fb555a0e356442cd534de
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-25 12:13:54 +0200

    fix after livecoding

--- snack1.js
+++ snack1.js
@@ -17,8 +17,9 @@ const bicycles = [
     },
 ]
 
-let weigthValues = [];
-
+/* let weigthValues = [];
+const {weight} = bicycles;
+console.log({weight}.weight);
 for (let i = 0; i < bicycles.length; i++) {
     const {weight} = bicycles[i];
     weigthValues.push({weight}.weight);
@@ -26,9 +27,26 @@ for (let i = 0; i < bicycles.length; i++) {
 
 // console.log(...weigthValues);
 const min = Math.min(...weigthValues);
+
 // console.log(min);
 
 //DOM
 const h1 = document.createElement('h1');
 document.querySelector('body').append(h1)
-h1.innerHTML = 'La bici più leggera pesa: ' + min + 'kg';
+h1.innerHTML = 'La bici più leggera pesa: ' + min + 'kg'; */
+
+
+
+//Prendere una bici di riferimento
+let lightestBike = bicycles[0];
+
+//ciclare tra le bici
+for (let i = 0; i < bicycles.length; i++) {
+    const bike = bicycles[i];
+    
+    if(bike.weight < lightestBike.weight){
+        lightestBike = bike;
+    }
+}
+
+document.querySelector('body').innerHTML = `La bici più leggera è la ${lightestBike.name} e pesa ${lightestBike.weight}kg`
