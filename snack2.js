a915804b7d2a9b89daa20eab195b98eb87b85211
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-25 12:39:38 +0200

    JSnack2 select points and fouls inside objects

--- snack2.js
+++ snack2.js
@@ -24,4 +24,30 @@ const footballTeams = [
         points: 0,
         fouls: 0,
     },
+    {
+        name: 'Napoli',
+        points: 0,
+        fouls: 0,
+    },
 ]
+
+//generare numeri random al posto degli 0 nelle proprietà: points e fouls.
+for (const key in footballTeams) {
+    console.log(footballTeams[key].points);
+    console.log(footballTeams[key].fouls);
+}
+
+
+
+
+/**
+ * Funzione da MDN per la generazione di numeri random
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns 
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
