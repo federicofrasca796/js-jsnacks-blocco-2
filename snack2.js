01b45f30c82784f3dc789981d63c0bf87a3b5ef9
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-25 12:19:31 +0200

    Snack 2 PartA completed

---
+++ snack2.js
@@ -0,0 +1,27 @@
+const footballTeams = [
+    {
+        name: 'Juventus',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Barcellona',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Manchester United',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Milan',
+        points: 0,
+        fouls: 0,
+    },
+]
